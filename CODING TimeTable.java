3c85dfff2318359f426160a02d32c9300b0e2737
AHMAD ASYRAAF DANIAL BIN NORAZLAN 284368 <92504346+AsyraafDanial-284368@users.noreply.github.com>
2022-01-17 14:10:16 +0800

    Update CODING TimeTable.java

--- CODING TimeTable.java
+++ CODING TimeTable.java
@@ -19,19 +19,19 @@ public class TimeTable {
 		 for(int i = -1; i <= n.length; i++) 
 	        {
 	            if (i == -1) {
-	                System.out.print("\t|");
+	                System.out.print("   *\t|");
 	            }
 	            else if (i == 0) {
 	                System.out.print("------------");
 	            }
 	            else if (i > 0) {                    
-	                System.out.printf("%1$d\t|", i);    
+	                System.out.printf("%4d\t|", i);    
 	            }
 	            
 	            for(int j = 1; j <= n.length; j++)
 	            {        
 	                if (i == -1) {
-	                    System.out.printf("\t%1$d", j);
+	                    System.out.printf("\t%4d", j);
 	                    continue;
 	                }
 	                else if (i == 0) {
@@ -39,7 +39,7 @@ public class TimeTable {
 	                    continue;
 	                }
 	                
-	                System.out.printf("\t%1$d", i * j);
+	                System.out.printf("\t%4d", i * j);
 	            }
 	            System.out.println();
 	        }
